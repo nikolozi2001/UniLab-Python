1d6312905721c05a7fe78ae58b40c3ab1a82b2a8
nikolozi2001 <mr.qachibaia44@gmail.com>
2023-05-11 16:52:51 +0400

    added lesson 9

---
+++ Lesson's/loops.py
@@ -0,0 +1,15 @@
+text = "Python"
+counter = 0
+
+
+# for i in text:
+#     # print(i)
+#     print(f"letter: {i}, index: {text.index(i)}")
+#     counter += 1    
+
+# print(counter)
+    
+
+while counter < len(text):
+    print(f"{text[counter]}")
+    counter += 1
